e6c84b6a5cfe0824ba1e0a443d095f80230090d4
Terry <terrywan117@gmail.com>
2024-04-29 20:24:40 -0400

    Add Budget and Save new Budget functionality

--- src/View/Dashboard.java
+++ src/View/Dashboard.java
@@ -1,11 +1,9 @@
 package src.View;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
-import java.util.Vector;
 
 public class Dashboard {
     private JPanel contentPane;
@@ -15,7 +13,7 @@ public class Dashboard {
     private JLabel transactionTitle;
     private JButton addAccButton;
     private JButton withdrawButton;
-    private JButton editBudgetButton;
+    private JButton addBudgetButton;
     private JTable budgetTable;
     private JPanel budgetViewPanel;
     private JButton depositButton;
@@ -28,6 +26,12 @@ public class Dashboard {
     private JLabel routingNum;
     private JLabel totalBalance;
     private JButton refreshInfoButton;
+    private JTextField tfRent;
+    private JTextField tfBills;
+    private JTextField tfGroceries;
+    private JTextField tfSavings;
+    private JTextField tfChecking;
+    private JButton saveBudgetButton;
 
     public void showDashboard() {
 
@@ -44,7 +48,7 @@ public class Dashboard {
         frame.setVisible(true);
 
         // Add action listener for the "Edit Budget" button
-        editBudgetButton.addActionListener(new ActionListener() {
+        addBudgetButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // Create and display the budget creation GUI
                 BudgetCreation budgetCreation = new BudgetCreation();
@@ -54,6 +58,48 @@ public class Dashboard {
                 budgetFrame.pack();
                 budgetFrame.setVisible(true);
                 budgetFrame.setLocationRelativeTo(null);
+            }
+        });
+
+        saveBudgetButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = Login.getCurrentUser();
+                Double rent = Double.parseDouble(tfRent.getText());
+                Double bills = Double.parseDouble(tfBills.getText());
+                Double groceries = Double.parseDouble(tfGroceries.getText());
+                Double savings = Double.parseDouble(tfSavings.getText());
+                Double checking = Double.parseDouble(tfChecking.getText());
+                try {
+                    // Establish the connection to the database
+                    Connection connection = DriverManager.getConnection("jdbc:ucanaccess://src/bankdb.accdb");
+
+                    // Prepare SQL statement
+                    String sql = "update Budgets set Rent = ?, Bills = ? , Groceries = ? , Savings = ? , Checking = ? where Budgets.Username = ?";
+                    PreparedStatement statement = connection.prepareStatement(sql);
+                    statement.setDouble(1, rent);
+                    statement.setDouble(2, bills);
+                    statement.setDouble(3, groceries);
+                    statement.setDouble(4, savings);
+                    statement.setDouble(5, checking);
+                    statement.setString(6, username);
+
+                    // Execute the statement
+                    statement.executeUpdate();
+                    JOptionPane.showMessageDialog(frame, "New Budget saved!");
+                    tfRent.setText(String.valueOf(rent));
+                    tfBills.setText(String.valueOf(bills));
+                    tfGroceries.setText(String.valueOf(groceries));
+                    tfSavings.setText(String.valueOf(savings));
+                    tfChecking.setText(String.valueOf(checking));
+
+                    // Close resources
+                    statement.close();
+                    connection.close();
+
+                }
+                catch (SQLException ex) {
+                    System.err.println("Error: " + ex.getMessage());
+                }
             }
         });
 
@@ -69,7 +115,7 @@ public class Dashboard {
         // Add action listener for the "Refresh Info" button
         refreshInfoButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                populateBudgetTable();
+                populateBudget();
                 String username = Login.getCurrentUser();
 
                 try {
@@ -126,7 +172,7 @@ public class Dashboard {
         });
     }
 
-    public void populateBudgetTable() {
+    public void populateBudget() {
         String username = Login.getCurrentUser(); // Get the current user
 
         try (Connection conn = DriverManager.getConnection("jdbc:ucanaccess://src/bankdb.accdb")) {
@@ -135,38 +181,27 @@ public class Dashboard {
                 pstmt.setString(1, username);
                 try (ResultSet rs = pstmt.executeQuery()) {
                     if (rs.next()) {
-                        budgetTable.setModel(buildTableModel(rs));
+                        double rent = rs.getDouble("Rent");
+                        double bills = rs.getDouble("Bills");
+                        double groceries = rs.getDouble("Groceries");
+                        double savings = rs.getDouble("Savings");
+                        double checking = rs.getDouble("Checking");
+                        System.out.println("Budget found!");
+                        tfRent.setText(String.valueOf(rent));
+                        tfBills.setText(String.valueOf(bills));
+                        tfGroceries.setText(String.valueOf(groceries));
+                        tfSavings.setText(String.valueOf(savings));
+                        tfChecking.setText(String.valueOf(checking));
+                    } else {
+                        System.err.println("User has no budget.");
                     }
+                    pstmt.close();
+                    conn.close();
                 }
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
             // Handle any SQL exceptions
         }
-    }
-
-    public static DefaultTableModel buildTableModel(ResultSet rs)
-            throws SQLException {
-
-        ResultSetMetaData metaData = rs.getMetaData();
-
-        // names of columns
-        Vector<String> columnNames = new Vector<>();
-        int columnCount = metaData.getColumnCount();
-        for (int column = 1; column <= columnCount; column++) {
-            columnNames.add(metaData.getColumnName(column));
-        }
-
-        // data of the table
-        Vector<Vector<Object>> data = new Vector<>();
-        while (rs.next()) {
-            Vector<Object> vector = new Vector<>();
-            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-                vector.add(rs.getObject(columnIndex));
-            }
-            data.add(vector);
-        }
-
-        return new DefaultTableModel(data, columnNames);
     }
 }
