2c0a1efaf10d9e2f5deb3f03f0e74da2723fa559
Terry <terrywan117@gmail.com>
2024-04-29 20:58:31 -0400

    Saving new budget functionality

--- src/View/Dashboard.java
+++ src/View/Dashboard.java
@@ -1,22 +1,19 @@
 package src.View;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
-import java.util.Vector;
 
 public class Dashboard {
     private JPanel contentPane;
     private JLabel budgetTitle;
-    private JPanel budgetFrame;
+    private JPanel budgetPanel;
     private JPanel transactionPanel;
     private JLabel transactionTitle;
     private JButton addAccButton;
     private JButton withdrawButton;
     private JButton addBudgetButton;
-    private JTable budgetTable;
     private JPanel budgetViewPanel;
     private JButton depositButton;
     private JLabel accNameTitle;
@@ -28,36 +25,34 @@ public class Dashboard {
     private JLabel routingNum;
     private JLabel totalBalance;
     private JButton refreshInfoButton;
-    private JButton refreshBudgetButton;
-    private JButton editBudgetButton;
-    private JPanel budgetPanel;
-    private JLabel rentLabel;
-    private JLabel rentAmountLabel;
-    private JLabel billsLabel;
-    private JLabel billsAmountLabel;
-    private JLabel groceriesLabel;
-    private JLabel groceriesAmountLabel;
-    private JLabel savingsLabel;
-    private JLabel savingsAmountLabel;
-    private JLabel checkingsLabel;
-    private JLabel checkingsAmountLabel;
+    private JTextField tfRent;
+    private JTextField tfBills;
+    private JTextField tfGroceries;
+    private JTextField tfSavings;
+    private JTextField tfChecking;
+    private JButton saveBudgetButton;
+    private JPanel budgetFrame;
 
     public void showDashboard() {
+
         // Create and configure the main frame for the dashboard
         JFrame frame = new JFrame("Dashboard");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
+
         // Set content pane of the frame
         frame.setContentPane(contentPane);
+
         // Make the frame visible
         frame.setVisible(true);
+
         // Add action listener for the "Edit Budget" button
         addBudgetButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // Create and display the budget creation GUI
                 BudgetCreation budgetCreation = new BudgetCreation();
-                JFrame budgetFrame = new JFrame("Budget");
+                JFrame budgetFrame = new JFrame("Edit Budget");
                 budgetFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 budgetFrame.setContentPane(budgetCreation.BudgetCreation);
                 budgetFrame.pack();
@@ -65,6 +60,49 @@ public class Dashboard {
                 budgetFrame.setLocationRelativeTo(null);
             }
         });
+
+        saveBudgetButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = Login.getCurrentUser();
+                Double rent = Double.parseDouble(tfRent.getText());
+                Double bills = Double.parseDouble(tfBills.getText());
+                Double groceries = Double.parseDouble(tfGroceries.getText());
+                Double savings = Double.parseDouble(tfSavings.getText());
+                Double checking = Double.parseDouble(tfChecking.getText());
+                try {
+                    // Establish the connection to the database
+                    Connection connection = DriverManager.getConnection("jdbc:ucanaccess://src/bankdb.accdb");
+
+                    // Prepare SQL statement
+                    String sql = "update Budgets set Rent = ?, Bills = ? , Groceries = ? , Savings = ? , Checking = ? where Budgets.Username = ?";
+                    PreparedStatement statement = connection.prepareStatement(sql);
+                    statement.setDouble(1, rent);
+                    statement.setDouble(2, bills);
+                    statement.setDouble(3, groceries);
+                    statement.setDouble(4, savings);
+                    statement.setDouble(5, checking);
+                    statement.setString(6, username);
+
+                    // Execute the statement
+                    statement.executeUpdate();
+                    JOptionPane.showMessageDialog(frame, "New Budget saved!");
+                    tfRent.setText(String.valueOf(rent));
+                    tfBills.setText(String.valueOf(bills));
+                    tfGroceries.setText(String.valueOf(groceries));
+                    tfSavings.setText(String.valueOf(savings));
+                    tfChecking.setText(String.valueOf(checking));
+
+                    // Close resources
+                    statement.close();
+                    connection.close();
+
+                }
+                catch (SQLException ex) {
+                    System.err.println("Error: " + ex.getMessage());
+                }
+            }
+        });
+
         // Add action listener for the "Add Bank Account" button
         addAccButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -73,10 +111,11 @@ public class Dashboard {
                 menu.setVisible(true);
             }
         });
+
         // Add action listener for the "Refresh Info" button
         refreshInfoButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                populateBudgetTable();
+                populateBudget();
                 String username = Login.getCurrentUser();
 
                 try {
@@ -115,6 +154,7 @@ public class Dashboard {
                 }
             }
         });
+
         depositButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Deposit menu = new Deposit();
@@ -122,6 +162,7 @@ public class Dashboard {
                 menu.setVisible(true);
             }
         });
+
         withdrawButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Withdraw menu = new Withdraw();
@@ -129,48 +170,9 @@ public class Dashboard {
                 menu.setVisible(true);
             }
         });
-        editBudgetButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                DefaultTableModel tableModel = new DefaultTableModel(
-                        new Object[][]{{"Test", 0.0}, {"Test", 0.0}, {"Test", 0.0},
-                        {"Test", 0.0}, {"Test", 0.0}}, new String[]{"Category", "Amount"});
-            }
-        });
-        refreshBudgetButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String userName = Login.getCurrentUser();
-                try {
-                    Connection connection = DriverManager.getConnection("jdbc:ucanaccess://src/bankdb.accdb");
-                    String sql = "select Rent, Bills, Groceries, Savings, Checking from Budgets where Budgets.Username = ?";
-                    PreparedStatement statement = connection.prepareStatement(sql);
-                    statement.setString(1, userName);
-                    ResultSet budgetInfoResults = statement.executeQuery();
-                    if (budgetInfoResults.next()) {
-                        int rentAmount = budgetInfoResults.getInt("Rent");
-                        int billsAmount = budgetInfoResults.getInt("Bills");
-                        int groceriesAmount = budgetInfoResults.getInt("Groceries");
-                        int savingsAmount = budgetInfoResults.getInt("Savings");
-                        int checkingAmount = budgetInfoResults.getInt("Checking");
-                        System.out.println("User found!");
-                        rentAmountLabel.setText(String.valueOf(rentAmount));
-                        billsAmountLabel.setText(String.valueOf(billsAmount));
-                        groceriesAmountLabel.setText(String.valueOf(groceriesAmount));
-                        savingsAmountLabel.setText(String.valueOf(savingsAmount));
-                        checkingsAmountLabel.setText(String.valueOf(checkingAmount));
-                    }
-                    else {
-                        System.err.println("No such user was found.");
-                        JOptionPane.showMessageDialog(frame, "No budget was found.");
-                    }
-                }
-                catch (SQLException ex){
-                    System.err.println("Error: " + ex.getMessage());
-                }
-            }
-        });
     }
 
-    public void populateBudgetTable() {
+    public void populateBudget() {
         String username = Login.getCurrentUser(); // Get the current user
 
         try (Connection conn = DriverManager.getConnection("jdbc:ucanaccess://src/bankdb.accdb")) {
@@ -179,34 +181,27 @@ public class Dashboard {
                 pstmt.setString(1, username);
                 try (ResultSet rs = pstmt.executeQuery()) {
                     if (rs.next()) {
-                        budgetTable.setModel(buildTableModel(rs));
+                        double rent = rs.getDouble("Rent");
+                        double bills = rs.getDouble("Bills");
+                        double groceries = rs.getDouble("Groceries");
+                        double savings = rs.getDouble("Savings");
+                        double checking = rs.getDouble("Checking");
+                        System.out.println("Budget found!");
+                        tfRent.setText(String.valueOf(rent));
+                        tfBills.setText(String.valueOf(bills));
+                        tfGroceries.setText(String.valueOf(groceries));
+                        tfSavings.setText(String.valueOf(savings));
+                        tfChecking.setText(String.valueOf(checking));
+                    } else {
+                        System.err.println("User has no budget.");
                     }
+                    pstmt.close();
+                    conn.close();
                 }
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
             // Handle any SQL exceptions
         }
-    }
-
-    public static DefaultTableModel buildTableModel(ResultSet rs)
-            throws SQLException {
-        ResultSetMetaData metaData = rs.getMetaData();
-        // names of columns
-        Vector<String> columnNames = new Vector<>();
-        int columnCount = metaData.getColumnCount();
-        for (int column = 1; column <= columnCount; column++) {
-            columnNames.add(metaData.getColumnName(column));
-        }
-        // data of the table
-        Vector<Vector<Object>> data = new Vector<>();
-        while (rs.next()) {
-            Vector<Object> vector = new Vector<>();
-            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-                vector.add(rs.getObject(columnIndex));
-            }
-            data.add(vector);
-        }
-        return new DefaultTableModel(data, columnNames);
     }
 }
--- src/View/NumBudget.java
+++
@@ -1,8 +0,0 @@
-package src.View;
-
-public class NumBudget implements BudgetDivision {
-    @Override
-    public void createBudget() {
-        System.out.println("Creating a dollar-based budget.");
-    }
-}
